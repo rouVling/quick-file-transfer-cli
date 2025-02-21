21496bdedc6f7b81afaf1070c42dec14ba891d14
rouVling <youqianiqian@126.com>
2025-02-21 18:32:30 +0800

    update profile feature

--- fetch.sh
+++ fetch.sh
@@ -2,21 +2,37 @@
 
 # 设置默认的配置文件路径
 CONFIG_FILE="$HOME/.fetchrc"
+CONFIG_FOLDER="$HOME/.fetch_profile"
 
 # 检查配置文件是否存在，如果不存在则创建并设置默认值
 if [ ! -f "$CONFIG_FILE" ]; then
     touch "$CONFIG_FILE"
-    echo "# Default server URL" >> "$CONFIG_FILE"
     echo "
-server_url=\"http://localhost:5000\"
-fetch_password=\"default_password\"
+profile=\"default\"
 " >> "$CONFIG_FILE"
 fi
+source "$CONFIG_FILE"
+export profile
+
+if [ ! -d "$CONFIG_FOLDER" ]; then
+    mkdir "$CONFIG_FOLDER"
+fi
+
+if [ ! -f "$CONFIG_FOLDER/$profile" ]; then
+    touch "$CONFIG_FOLDER/$profile"
+    echo "# Default server URL" >> "$CONFIG_FOLDER/$profile"
+    echo "
+server_url=\"http://localhost\"
+fetch_password=\"password\"
+port=\"9963\"
+" >> "$CONFIG_FOLDER/$profile"
+fi
+source "$CONFIG_FOLDER/$profile"
 
 # 从配置文件中读取服务器 URL
-source "$CONFIG_FILE"
 export server_url
 export fetch_password
+export port
 password=$(echo -n "${fetch_password}$(date +%s | cut -c -9)" | sha256sum | cut -d' ' -f1)
 
 # 检查命令参数
@@ -34,7 +50,8 @@ case "$command" in
         for file in "${@:2}"; do
             if [ -f "$file" ]; then
                 # wget --post-file "$file" "$server_url/upload"
-                curl --fail --header "Authorization: $password" -X POST -F "file=@$file" "$server_url/upload" || echo "Failed to upload $file"
+                password=$(echo -n "${fetch_password}$(date +%s | cut -c -9)" | sha256sum | cut -d' ' -f1)
+                curl --fail --header "Authorization: $password" -X POST -F "file=@$file" "$server_url:$port/upload" || echo "Failed to upload $file"
             else
                 echo "File $file not found"
             fi
@@ -56,10 +73,10 @@ case "$command" in
             fi
             if $no_remove; then
                 # wget "$server_url/download/$file" -O "$file"
-                curl --fail --header "Authorization: $password" -X GET "$server_url/download/$file" -o "$file" || echo "Failed to download $file"
+                curl --fail --header "Authorization: $password" -X GET "$server_url:$port/download/$file" -o "$file" || echo "Failed to download $file"
             else
                 # wget "$server_url/download/$file" -O "$file"
-                curl --fail --header "Authorization: $password" -X GET "$server_url/download/$file" -o "$file" && curl --header "Authorization: $password" -X POST "$server_url/remove/$file" || echo "Failed to download $file"
+                curl --fail --header "Authorization: $password" -X GET "$server_url:$port/download/$file" -o "$file" && curl --header "Authorization: $password" -X POST "$server_url:$port/remove/$file" || echo "Failed to download $file"
                 # wget "$server_url/remove/$file" -O "$file"
                 # then post to delete the file
                 # curl --header "Authorization: $password" -X POST "$server_url/remove/$file"
@@ -72,7 +89,8 @@ case "$command" in
             echo "
 server_url=\"$2\"
 fetch_password=\"$fetch_password\"
-" > "$CONFIG_FILE"
+port=\"$port\"
+" > "$CONFIG_FOLDER/$profile"
             echo "Server URL set to $2"
         else
             echo "No URL provided for source"
@@ -86,18 +104,57 @@ fetch_password=\"$fetch_password\"
             echo "
 server_url=\"$server_url\"
 fetch_password=\"$2\"
-" > "$CONFIG_FILE"
+port=\"$port\"
+" > "$CONFIG_FOLDER/$profile"
             echo "password set to $2"
         else
             echo "No URL provided for source"
             exit 1
         fi
         ;;
+    port)
+        # 设置端口
+        if [ -n "$2" ]; then
+            echo "
+server_url=\"$server_url\"
+fetch_password=\"$fetch_password\"
+port=\"$2\"
+" > "$CONFIG_FOLDER/$profile"
+            echo "Port set to $2"
+        else
+            echo "No port provided"
+            exit 1
+        fi
+        ;;
+    profile)
+        # 设置配置文件
+        if [ -n "$2" ]; then
+            echo "
+profile=\"$2\"
+" > "$CONFIG_FILE"
+            if [ -f "$CONFIG_FOLDER/$2" ]; then
+                echo "Switching to profile $2"
+            else
+                touch "$CONFIG_FOLDER/$2"
+                echo "# Default server URL" >> "$CONFIG_FOLDER/$2"
+                echo "
+server_url=\"http://localhost\"
+fetch_password=\"password\"
+port=\"9963\"
+" >> "$CONFIG_FOLDER/$2"
+                echo "Profile $2 created"
+            fi
+        else
+            echo "No profile provided"
+            exit 1
+        fi
+    ;;
     test)
         echo "server_url: $server_url"
+        echo "port: $port"
         echo "fetch_password: $fetch_password"
         echo "token: $password"
-        curl --header "Authorization: $password" -X GET "$server_url/test"
+        curl --header "Authorization: $password" -X GET "$server_url:$port/test"
         # curl -X POST "$server_url/test"
     ;;
 
